8a19d974eceef807ab22985ff3caed1fef69dae1
javaz-t <147338963+javaz-t@users.noreply.github.com>
2023-11-17 19:28:04 +0530

    color changed

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoApp(
       title: 'Itetwpp',
-      home: CupertinoPageScaffold(child: Container(color:Colors.red)),
+      home: CupertinoPageScaffold(child: Container(color:Colors.blue)),
     );
   }
 }
